e17aedf3e75f2b00d8024bed2aea9d3b58d6da85
luotuo450 <1153432805@qq.com>
2025-02-03 13:53:40 +0800

    修改了deepseek.py文件

--- pythonprojects/deepseek.py
+++ pythonprojects/deepseek.py
@@ -1,9 +1,12 @@
 import requests
+import os
 
+print("您的模型有：")
+os.system("ollama list")
+model = input("请选择模型：")
 host = "xxx"
 port = "xxx"
 url = f"http://127.0.0.1:11434/api/chat"  # 更改成你的接口地址
-model = "deepseek-r1:1.5b"
 headers = {"Content-Type": "application/json"}
 user = 0
 while user != "exit":
@@ -13,7 +16,7 @@ while user != "exit":
     data = {
         "model": model,  # 模型选择
         "options": {
-            "temperature": 1  # 为0表示不让模型自由发挥，输出结果相对较固定，>0的话，输出的结果会比较放飞自我
+            "temperature": 0  # 为0表示不让模型自由发挥，输出结果相对较固定，>0的话，输出的结果会比较放飞自我
         },
         "stream": False,  # 流式输出
         "messages": [{"role": "system", "content": user}],  # 对话列表
